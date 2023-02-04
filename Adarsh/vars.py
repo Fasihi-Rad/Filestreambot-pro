5e64d190e3fe9720775faa4f49b59b82da4e6870
AmirReza FasihiRad <60557634+Fasihi-Rad@users.noreply.github.com>
2023-02-05 02:04:26 +0330

    Update Vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -34,7 +34,8 @@ class Var(object):
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
-        URL = "http://{}/".format(FQDN)
+        URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
+            "http://{}:{}/".format(FQDN, PORT)
     DATABASE_URL = str(getenv('DATABASE_URL'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
